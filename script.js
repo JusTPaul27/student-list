d5cb71777ccc2bf2d9378969876432f949e12649
ChristopherL <101825137+ChristopherL-creator@users.noreply.github.com>
2022-04-29 16:50:20 +0200

    u

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
 // lista studenti
 
-// solo ragazze
-
-// solo ragazzi
-
-// sorting di qualche genere
-
 const corso1 = {
   nome: "programmatore front-end",
   ore: 300,
@@ -109,7 +103,7 @@ const studente10 = {
   cognome: "San Giorgio",
   genere: "maschio",
   eta: 26,
-  citta: "Lima",
+  citta: "San Pietroburgo",
   sposato: false,
   corso: corso2,
 };
@@ -139,7 +133,6 @@ const studenti = [
 function writeStudentsDocument(studenti) {
   const studentContainer = document.getElementById("student-container");
 
-  //  creo una lista ordinata:
   for (let i = 0; i < studenti.length; i++) {
     const studente = studenti[i];
 
@@ -151,20 +144,130 @@ function writeStudentsDocument(studenti) {
 
     p.appendChild(nome);
 
-    p.appendChild(br); 
-
-    const cognome = document.createTextNode(' ' + studente.cognome);
+    const cognome = document.createTextNode(' ' + studente.cognome + ';');
 
     p.appendChild(cognome); 
     
     p.appendChild(br);
 
-    const genere = document.createTextNode('genere: ' + studente.genere);
+    const genere = document.createTextNode('genere: ' + studente.genere + ';'); 
 
-    p.appendChild(genere);
+    const br2 = document.createElement("br");
 
-    studentContainer.appendChild(p);
+    p.appendChild(genere); 
+
+    p.appendChild(br2);
+
+    const eta = document.createTextNode('età: ' + studente.eta + ';'); 
+
+    const br3 = document.createElement("br");
+
+    p.appendChild(eta); 
+
+    p.appendChild(br3); 
+
+    const città = document.createTextNode('città: ' + studente.citta + ';'); 
+
+    const br4 = document.createElement("br");
+
+    p.appendChild(città); 
+
+    p.appendChild(br4);
+
+    const sposato = document.createTextNode('sposato: ' + studente.sposato + ';'); 
+
+    const br5 = document.createElement("br");
+
+    p.appendChild(sposato); 
+
+    p.appendChild(br5); 
+
+    const corso = document.createTextNode('corso: ' + JSON.stringify(studente.corso.nome) + ';'); 
+
+    const br6 = document.createElement("br");
+
+    p.appendChild(corso); 
+
+    p.appendChild(br6);
+
+    studentContainer.appendChild(p); 
+
   }
 }
 
 writeStudentsDocument(studenti);
+
+// solo ragazze; 
+
+function writeFemaleDocument(studenti) {
+  const femaleContainer = document.getElementById("female-container"); 
+
+  // for (let i = 0; i < studenti.length; i++) { 
+  //   const studente = studenti[i]; 
+    
+  //   const p = document.createElement("p"); 
+
+  //   const nome = document.createTextNode(studente.nome);
+
+  //   const br = document.createElement("br");
+
+  //   p.appendChild(nome);
+
+  //   const cognome = document.createTextNode(' ' + studente.cognome + ';');
+
+  //   p.appendChild(cognome); 
+    
+  //   p.appendChild(br);
+
+  //   const genere = document.createTextNode('genere: ' + studente.genere + ';'); 
+
+  //   const br2 = document.createElement("br");
+
+  //   p.appendChild(genere); 
+
+  //   p.appendChild(br2);
+
+  //   const eta = document.createTextNode('età: ' + studente.eta + ';'); 
+
+  //   const br3 = document.createElement("br");
+
+  //   p.appendChild(eta); 
+
+  //   p.appendChild(br3); 
+
+  //   const città = document.createTextNode('città: ' + studente.citta + ';'); 
+
+  //   const br4 = document.createElement("br");
+
+  //   p.appendChild(città); 
+
+  //   p.appendChild(br4);
+
+  //   const sposato = document.createTextNode('sposato: ' + studente.sposato + ';'); 
+
+  //   const br5 = document.createElement("br");
+
+  //   p.appendChild(sposato); 
+
+  //   p.appendChild(br5); 
+
+  //   const corso = document.createTextNode('corso: ' + JSON.stringify(studente.corso.nome) + ';'); 
+
+  //   const br6 = document.createElement("br");
+
+  //   p.appendChild(corso); 
+
+  //   p.appendChild(br6);
+
+  //   femaleContainer.appendChild(p); 
+
+  // } 
+
+  const IsFemale = studenti.filter( => v.name === "Joe" && v.age < 30);
+}
+
+writeFemaleDocument(studenti);
+
+// solo ragazzi
+
+// sorting di qualche genere
